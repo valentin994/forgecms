46e9c22696d14bff758123ff85f78eeaac26b756
Valentin Vareskic <valentin.vareskic@gmail.com>
2024-04-08 16:26:19 +0200

    enabled logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,26 +4,28 @@ use axum::{
     Json, Router,
 };
 use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     // initialize tracing
-    tracing_subscriber::fmt::init();
-
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
         .route("/users", post(create_user));
-
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
+    tracing::debug!("hello");
     "Hello, World!"
 }
 
