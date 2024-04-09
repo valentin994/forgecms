db3a9685c6bb631822816279283d0ec4312dd777
valentin994 <valentin.vareskic@gmail.com>
2024-04-09 14:29:39 +0200

    added first test

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,6 @@
 use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    Json, Router,
+    routing::get, Router,
 };
-use serde::{Deserialize, Serialize};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -15,9 +12,8 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root))
+        .route("/", get(root));
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
@@ -29,31 +25,3 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
---
+++ backend/tests/main.rs
@@ -0,0 +1,12 @@
+#[tokio::test]
+async fn test_root() {
+    let client = reqwest::Client::new();
+    let res = client.get("http://localhost:3000/")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    println!("{}", res);
+}
