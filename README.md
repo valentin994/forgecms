31cc587969f9a00ef9116bd95218e6eca1fd21af
valentin994 <valentin.vareskic@gmail.com>
2024-04-10 14:03:49 +0200

    added migration, added database to main.rs, updated readme

--- README.md
+++ README.md
@@ -1,9 +1,47 @@
 ForgeCMS
 ---
-A lightning fast rust based CMS. 
+⚡A lightning fast rust based CMS. ⚡
 
-Contributions
----
+## Installation
+The CMS is built with axum for managing the requests, and with sqlx & slqx-cli for managing the database.
+Out of the box there will be a review table that is initilized with the server. There is a docker compose
+set up to bootstrap everything, but if you already have a setup database you can run the server independently
+wih: 
+
+```sh
+$ cd ./backend
+$ cargo run --release
+```
+
+Or if you want to bootstrap from scratch use:
+```sh
+$ docker compose up
+```
+If you want the containers to run in the background run with `-d` which is detached mode:
+```sc
+$ docker compose up -d
+```
+
+To strip everything down run:
+```sh 
+$ docker compose down
+```
+
+If you do any changes to the code and want to apply those changes, run:
+```sh 
+$ docker compose build
+$ docker compose up
+```
+
+## Database
+This project uses postgres as its database of choice. 
+When starting the application you will have these tables created:
+- reviews
+
+If you want to add more tables you can do that with the [sqlx-cli](https://crates.io/crates/sqlx-cli). 
+
+
+## Contributions
 If you want to contribute you are more than welcome. Please open a pull request and write tests for your PR.
 After all tests and conditions pass I'll take a look into it and merge it.
 
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,11 @@
 use axum::{routing::get, Router};
 use tokio::net::TcpListener;
+use sqlx::{migrate::Migrator, postgres::{PgPool, PgPoolOptions}};
+use std::time::Duration;
+
+const DB_MAX_CONNECTIONS: u32 = 10;
+const DB_CONNECTION_TIMEOUT: Duration = Duration::from_secs(5);
+static MIGRATOR: Migrator = sqlx::migrate!(); 
 
 #[tokio::main]
 async fn main() {
@@ -7,11 +13,24 @@ async fn main() {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .init();
+    // initialize database
+    let db_connection_str = std::env::var("localhost:5432")
+        .unwrap_or_else(|_| "postgres://postgres:mysecretpassword@localhost:5432".to_string());
+    let pool = PgPoolOptions::new()
+        .max_connections(DB_MAX_CONNECTIONS)
+        .acquire_timeout(DB_CONNECTION_TIMEOUT)
+        .connect(&db_connection_str)
+        .await
+        .expect("can't connect to database");
+
+    MIGRATOR.run(&pool).await.unwrap();
+    tracing::debug!("Database initialized and migrated");
+
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root));
-    // `POST /users` goes to `create_user`
+        .route("/", get(root))
+        .with_state(pool);
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
     tracing::debug!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
