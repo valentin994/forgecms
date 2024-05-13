66202199a925caaedc8edaa8821ca27eab847c40
Valentin Vareskic <valentin.vareskic@gmail.com>
2024-05-13 13:15:44 +0200

    added tests

--- backend/src/routes/reviews.rs
+++ backend/src/routes/reviews.rs
@@ -1,6 +1,11 @@
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
 use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPool;
-use axum::{routing::{get, post}, Router, Json, http::StatusCode, extract::Path};
 
 pub async fn review_router(pool: PgPool) -> Router {
     Router::new()
@@ -9,22 +14,22 @@ pub async fn review_router(pool: PgPool) -> Router {
         .with_state(pool)
 }
 
-async fn create_review(Json(_payload):Json<CreateReview>) -> (StatusCode, Json<Review>){
+async fn create_review(Json(_payload): Json<CreateReview>) -> (StatusCode, Json<Review>) {
     let review = Review {
         id: 1,
         name: "Hello".to_string(),
-        review: "World".to_string()
+        review: "World".to_string(),
     };
 
     tracing::debug!("Created a review!");
     (StatusCode::CREATED, Json(review))
 }
 
-async fn read_review(Path(id): Path<u64>) -> (StatusCode, Json<Review>){
+async fn read_review(Path(id): Path<u64>) -> (StatusCode, Json<Review>) {
     let review = Review {
         id: 1,
         name: "Hello".to_string(),
-        review: "World".to_string()
+        review: "World".to_string(),
     };
     tracing::debug!("Fetched review!");
     (StatusCode::OK, Json(review))
@@ -33,12 +38,12 @@ async fn read_review(Path(id): Path<u64>) -> (StatusCode, Json<Review>){
 #[derive(Deserialize)]
 struct CreateReview {
     name: String,
-    review: String
+    review: String,
 }
 
 #[derive(Serialize)]
 struct Review {
     id: u64,
     name: String,
-    review: String
+    review: String,
 }
--- backend/tests/main.rs
+++ backend/tests/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[tokio::test]
 async fn test_root() {
     let client = reqwest::Client::new();
@@ -9,5 +11,37 @@ async fn test_root() {
         .text()
         .await
         .unwrap();
-    println!("{}", res);
+    assert_eq!(res, "Hello, World!");
+}
+
+#[tokio::test]
+async fn get_review() {
+    let client = reqwest::Client::new();
+    let res = client
+        .get("http://127.0.0.1:3000/review/1")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    assert_eq!(res, "{\"id\":1,\"name\":\"Hello\",\"review\":\"World\"}")
+}
+
+#[tokio::test]
+async fn create_review() {
+    let mut map = HashMap::new();
+    map.insert("name", "hello"); 
+    map.insert("review", "world"); 
+    let client = reqwest::Client::new();
+    let res = client
+        .post("http://127.0.0.1:3000/review")
+        .json(&map)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    assert_eq!(res, "{\"id\":1,\"name\":\"Hello\",\"review\":\"World\"}")
 }
