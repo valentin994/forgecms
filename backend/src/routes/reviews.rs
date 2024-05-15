223e182cd01f7ee24f5ca07e9d03c1382911e203
valentin994 <valentin.vareskic@gmail.com>
2024-05-15 15:50:02 +0200

    added fetching all reviews, inserting a review and fetching a review by id

--- backend/src/routes/reviews.rs
+++ backend/src/routes/reviews.rs
@@ -10,16 +10,12 @@ use sqlx::postgres::PgPool;
 pub async fn review_router(pool: PgPool) -> Router {
     Router::new()
         .route("/", post(create_review))
+        .route("/", get(get_all_reviews))
         .route("/:id", get(read_review))
         .with_state(pool)
 }
 
 async fn create_review(State(pool): State<PgPool> ,Json(payload): Json<CreateReview>) -> (StatusCode, Json<Review>) {
-    let review = Review {
-        id: 1,
-        name: "Hello".to_string(),
-        review: "World".to_string(),
-    };
     tracing::debug!("Payload: name: {}, review: {}", &payload.name, &payload.review);
     let res = sqlx::query_as!(
         Review,
@@ -31,20 +27,40 @@ async fn create_review(State(pool): State<PgPool> ,Json(payload): Json<CreateRev
         payload.review
     )
     .fetch_one(&pool)
-    .await;
+    .await
+    .expect("Can't inesrt into database");
     tracing::debug!("{res:?}");
     tracing::debug!("Created a review!");
-    (StatusCode::CREATED, Json(review))
+    (StatusCode::CREATED, Json(res))
 }
 
-async fn read_review(Path(id): Path<u64>) -> (StatusCode, Json<Review>) {
-    let review = Review {
-        id: 1,
-        name: "Hello".to_string(),
-        review: "World".to_string(),
-    };
-    tracing::debug!("Fetched review!");
-    (StatusCode::OK, Json(review))
+async fn read_review(State(pool): State<PgPool>, Path(id): Path<i64>) -> (StatusCode, Json<Review>) {
+    tracing::debug!("Fetching review with id {}", &id);
+    let res = sqlx::query_as!(
+        Review,
+        r#"SELECT id, name, review FROM reviews WHERE id = $1"#,
+        id
+        )
+        .fetch_one(&pool)
+        .await
+        .expect("Can't fetch review");
+    tracing::debug!("Fetched review! - {res:?}");
+    (StatusCode::OK, Json(res))
+}
+
+
+async fn get_all_reviews(State(pool): State<PgPool>) -> (StatusCode, Json<Vec<Review>>) {
+    tracing::debug!("Getting all reviews");
+    let res = sqlx::query_as!(
+        Review,
+        r#"SELECT * FROM reviews"#
+        )
+        .fetch_all(&pool)
+        .await
+        .expect("");
+    tracing::debug!("Fetched all reviews!");
+    (StatusCode::OK, Json(res))
+
 }
 
 #[derive(Deserialize, Debug)]
