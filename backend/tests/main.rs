79bd8787cf30e3776609b54d568843c9b9bf56e0
Valentin Vareskic <valentin.vareskic@gmail.com>
2024-05-31 15:51:49 +0200

    fixed integration tests

--- backend/tests/main.rs
+++ backend/tests/main.rs
@@ -44,48 +44,105 @@ async fn create_review() {
     );
 }
 
-// #[tokio::test]
-// async fn get_review() {
-//     let client = reqwest::Client::new();
-//     let res = client
-//         .get("http://127.0.0.1:3000/review/1")
-//         .send()
-//         .await
-//         .unwrap()
-//         .text()
-//         .await
-//         .unwrap();
-//     assert_eq!(res, "{\"id\":1,\"name\":\"Hello\",\"review\":\"World\"}")
-// }
+#[tokio::test]
+async fn get_review() {
+    let mut map = HashMap::new();
+    map.insert("name", "hello");
+    map.insert("review", "world");
+    let client = reqwest::Client::new();
+    let created_review = client
+        .post("http://127.0.0.1:3000/review")
+        .json(&map)
+        .send()
+        .await
+        .expect("Should return created review.")
+        .json::<Review>()
+        .await
+        .unwrap();
+    let res = client
+        .get(format!(
+            "http://127.0.0.1:3000/review/{}",
+            created_review.id
+        ))
+        .send()
+        .await
+        .expect("Should return created review")
+        .json::<Review>()
+        .await
+        .unwrap();
+    assert_eq!(res, created_review)
+}
 
-// #[tokio::test]
-// async fn update_review() {
-//     let mut map = HashMap::new();
-//     map.insert("name", "hello");
-//     map.insert("review", "world2");
-//     let client = reqwest::Client::new();
-//     let res = client
-//         .patch("http://127.0.0.1:3000/review/1")
-//         .json(&map)
-//         .send()
-//         .await
-//         .unwrap()
-//         .text()
-//         .await
-//         .unwrap();
-//     assert_eq!(res, "{\"id\":1,\"name\":\"hello\",\"review\":\"world2\"}")
-// }
+#[tokio::test]
+async fn update_review() {
+    let mut map = HashMap::new();
+    map.insert("name", "hello");
+    map.insert("review", "world");
+    let client = reqwest::Client::new();
+    let created_review = client
+        .post("http://127.0.0.1:3000/review")
+        .json(&map)
+        .send()
+        .await
+        .expect("Should return created review.")
+        .json::<Review>()
+        .await
+        .unwrap();
 
-// #[tokio::test]
-// async fn delete_review() {
-//     let client = reqwest::Client::new();
-//     let res = client
-//         .delete("http://127.0.0.1:3000/review/1")
-//         .send()
-//         .await
-//         .unwrap()
-//         .text()
-//         .await
-//         .unwrap();
-//     assert_eq!(res, "Deleted review with id: 1".to_string())
-// }
+    let mut new_name = HashMap::new();
+    new_name.insert("name", "new");
+    let client = reqwest::Client::new();
+    let res = client
+        .patch(format!(
+            "http://127.0.0.1:3000/review/{}",
+            created_review.id
+        ))
+        .json(&new_name)
+        .send()
+        .await
+        .expect("Update the name")
+        .json::<Review>()
+        .await
+        .unwrap();
+    assert_eq!(
+        res,
+        Review {
+            id: created_review.id,
+            name: String::from("new"),
+            review: String::from("world")
+        }
+    );
+}
+
+#[tokio::test]
+async fn delete_review() {
+    let mut map = HashMap::new();
+    map.insert("name", "hello");
+    map.insert("review", "world");
+    let client = reqwest::Client::new();
+    let created_review = client
+        .post("http://127.0.0.1:3000/review")
+        .json(&map)
+        .send()
+        .await
+        .expect("Should return created review.")
+        .json::<Review>()
+        .await
+        .unwrap();
+
+    let res = client
+        .delete(format!(
+            "http://127.0.0.1:3000/review/{}",
+            created_review.id
+        ))
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    assert_eq!(
+        res,
+        format!("Deleted review with id: {}", created_review.id)
+    )
+}
